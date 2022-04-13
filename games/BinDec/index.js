210d7b267f1e0c3509771550362d20cf9505cf92
Wiwok <Emil.dechenaud@gmail.com>
2022-04-13 09:03:58 +0200

    Finishing binary games
    All games finished!

--- games/BinDec/index.js
+++ games/BinDec/index.js
@@ -2,12 +2,52 @@ import chalk from 'chalk';
 import promptSync from 'prompt-sync';
 const prompt = promptSync();
 
+function randomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function reverse(s) {
+	return s.split('').reverse().join('');
+}
+
+function DecToBin(decNumber) {
+	let hexNumber = decNumber % 2;
+	decNumber = Math.trunc(decNumber / 2);
+	while (decNumber != 0) {
+		hexNumber = hexNumber + '' + (decNumber % 2);
+		decNumber = Math.trunc(decNumber / 2);
+	}
+
+	hexNumber = reverse(hexNumber.toString());
+
+	/* for (let i = 0; i < hexNumber.toString().length() / 4; i++) {
+		hexNumber = hexNumber.toString()[4 ** i].split('').join(' ');
+	}*/
+
+	return hexNumber;
+}
 
 function BinDec() {
 
-	// Generate a random bin value and its dec number
+	const decNumber = randomInt(0, 65535);
+	const binNumber = DecToBin(decNumber);
 
-	// Tell the user the bin, and ask for the dec number
+	console.log('\n');
+	console.log('Binary number: ' + chalk.greenBright(binNumber));
+	console.log('What is the decimal equivalent ?');
+	let number = prompt('> ').toLowerCase();
+
+	while (number.includes(' ')) {
+		number = number.replace(' ', '');
+	}
+
+	if (decNumber.toString().toLowerCase() == number) {
+		console.log(chalk.green('Right!!'));
+		console.log('You\'re right, ' + chalk.redBright(decNumber) + ' is the correct answer.');
+	} else {
+		console.log(chalk.red('Wrong!!'));
+		console.log('The correct answer is: ' + chalk.redBright(decNumber));
+	}
 
 }
 
--- games/DecBin/index.js
+++ games/DecBin/index.js
@@ -3,15 +3,53 @@ import promptSync from 'prompt-sync';
 const prompt = promptSync();
 
 
+function randomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function reverse(s) {
+	return s.split('').reverse().join('');
+}
+
+function DecToBin(decNumber) {
+	let hexNumber = decNumber % 2;
+	decNumber = Math.trunc(decNumber / 2);
+	while (decNumber != 0) {
+		hexNumber = hexNumber + '' + (decNumber % 2);
+		decNumber = Math.trunc(decNumber / 2);
+	}
+
+	hexNumber = reverse(hexNumber.toString());
+
+	/* for (let i = 0; i < hexNumber.toString().length() / 4; i++) {
+		hexNumber = hexNumber.toString()[4 ** i].split('').join(' ');
+	}*/
+
+	return hexNumber;
+}
+
 function DecBin() {
 
-	const number = prompt('> ');
+	const decNumber = randomInt(0, 65535);
 
-	console.log(2 ** number);
+	const binNumber = DecToBin(decNumber);
 
-	// Generate a random dec number and its bin value
+	console.log('\n');
+	console.log('Decimal number: ' + chalk.redBright(decNumber));
+	console.log('What is the binary equivalent ?');
+	let number = prompt('> ').toLowerCase();
 
-	// Tell the user the dec, and ask for the bin value
+	while (number.includes(' ')) {
+		number = number.replace(' ', '');
+	}
+
+	if (binNumber.toString().toLowerCase() == number) {
+		console.log(chalk.green('Right!!'));
+		console.log('You\'re right, ' + chalk.greenBright(binNumber) + ' is the correct answer.');
+	} else {
+		console.log(chalk.red('Wrong!!'));
+		console.log('The correct answer is: ' + chalk.greenBright(binNumber));
+	}
 
 }
 
